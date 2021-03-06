16310f9f048e3b65db969b8a1a425e3873727637
Roman Basmanov <basmanovroman@mail.ru>
2021-03-06 20:23:23 +0500

    CRUD

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ client = app.test_client()
 
 tutorials = [
     {
+        'id': 1,
         'title': 'Video #1. Intro',
         'description': 'GET, POST routes'
     },
     {
+        'id': 2,
         'title': 'Video #2. More features',
         'description': 'GET, POST routes'
 
@@ -52,6 +54,41 @@ def update_list():
 # res.status_code
 # res.get_json()
 
+# Создаем новый роут для метода PUT
+@app.route('/tutorials/<int:tutorial_id>', methods=['PUT'])
+def update_tutorial(tutorial_id):
+    # Что бы получиться элемент по его id воспользуемся конструкцией генератором
+    # Он вернет элемент с соответствующим id или None
+    item = next((x for x in tutorials if x['id'] == tutorial_id), None)
+    # Нужно получиться параметры которые переданы в запросе для изменения
+    params = request.json
+    # Проверяем был ди найден соответствующий элемент в списке прежде чем изменять его
+    if not item:
+        return {'message': 'No tutorials with this id'}, 400
+    item.update(params)
+    # А в ответе мы вернем тот элемент списка который был изменен
+    return item
+
+# Протестируем роут в консоли питон.
+# from app import client
+# res = client.put('/tutorials/2', json={'description': 'PUT routes for editing'})
+# res.status_code
+# res.get_json()
+
+# Создаем новый роут для метода DELETE
+@app.route('/tutorials/<int:tutorial_id>', methods=['DELETE'])
+def delete_tutorial(tutorial_id):
+    # Получаем индекс элемента. Функция enumerate пронумеровывает словариб кортежи
+    idx, _ = next((x for x in enumerate(tutorials) if x[1]['id'] == tutorial_id), (None, None))
+    tutorials.pop(idx)
+    return '', 204
+
+# Протестируем роут в консоли питон.
+# from app import client
+# res = client.delete('/tutorials/1')
+# res.status_code должен быть код - 204
+# res = client.get('/tutorials')
+# res.get_json()
 
 
 if __name__ == '__main__':
